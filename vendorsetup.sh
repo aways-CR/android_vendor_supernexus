87605840635d568e473487ab975ad142891bf38c
Faryaab Sheikh <faryaabsheikh@gmail.com>
2013-08-07 23:39:53 +0500

    Cleanup vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,16 +1,1 @@
-add_lunch_combo captivatemtd-userdebug
-add_lunch_combo d2att-userdebug
-add_lunch_combo d2tmo-userdebug
-add_lunch_combo galaxysmtd-userdebug
-add_lunch_combo grouper-userdebug
-add_lunch_combo i777-userdebug
-add_lunch_combo i9100-userdebug
-add_lunch_combo i9100g-userdebug
 add_lunch_combo i9300-userdebug
-add_lunch_combo i9305-userdebug
-add_lunch_combo mako-userdebug
-add_lunch_combo n7000-userdebug
-add_lunch_combo n7100-userdebug
-add_lunch_combo t0lte-userdebug
-add_lunch_combo tilapia-userdebug
-add_lunch_combo vibrantmtd-userdebug
