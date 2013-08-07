4aa60dad949223ab5fe245aeb086a5699f4d477e
Faryaab Sheikh <faryaabsheikh@gmail.com>
2013-08-07 23:31:02 +0500

    Cleanup for Android 4.3

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,17 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/captivatemtd.mk \
-    $(LOCAL_DIR)/d2att.mk \
-    $(LOCAL_DIR)/d2tmo.mk \
-    $(LOCAL_DIR)/galaxysmtd.mk \
-    $(LOCAL_DIR)/grouper.mk \
-    $(LOCAL_DIR)/i777.mk \
-    $(LOCAL_DIR)/i9100.mk \
-    $(LOCAL_DIR)/i9100g.mk \
-    $(LOCAL_DIR)/i9300.mk \
-    $(LOCAL_DIR)/i9305.mk \
-    $(LOCAL_DIR)/mako.mk \
-    $(LOCAL_DIR)/n7000.mk \
-    $(LOCAL_DIR)/n7100.mk \
-    $(LOCAL_DIR)/t0lte.mk \
-    $(LOCAL_DIR)/tilapia.mk \
-    $(LOCAL_DIR)/vibrantmtd.mk
+    $(LOCAL_DIR)/i9300.mk
