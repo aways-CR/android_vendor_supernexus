4e8025a8f425cd44db436cefd17a4f07132bcb0f
Faryaab Sheikh <faryaabsheikh@gmail.com>
2013-08-11 21:14:18 +0500

    Add Samsung Galaxy S4 (jfltexx) Support

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,3 +1,4 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/i9300.mk \
-    $(LOCAL_DIR)/i9500.mk
+    $(LOCAL_DIR)/i9500.mk \
+    $(LOCAL_DIR)/jfltexx.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,3 @@
 add_lunch_combo i9300-userdebug
 add_lunch_combo i9500-userdebug
+add_lunch_combo jfltexx-userdebug
